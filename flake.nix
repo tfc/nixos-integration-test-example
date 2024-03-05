6a0c920175516bc461455ddbbcc35223ce9a00e1
Jacek Galowicz <jacek@galowicz.de>
2024-03-05 08:54:57 +0100

    Extend platforms

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [ "x86_64-linux" "aarch64-linux" ];
+    systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
     perSystem = { config, pkgs, ... }: {
       packages.default = pkgs.testers.runNixOSTest ./test.nix;
       packages.echo = pkgs.testers.runNixOSTest ./echo/test.nix;
